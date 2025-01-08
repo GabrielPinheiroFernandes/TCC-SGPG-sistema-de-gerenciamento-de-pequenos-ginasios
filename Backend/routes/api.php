bd5b4143589062e884f3132883eaff4fb010c1ff
gabrielpinheirofernandes <gab.pin.fer@hotmail.com>
2025-01-08 04:18:35 -0300

    add:/user/create endpoint | fix: DatabaseSeeder user(10)

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,7 +1,16 @@
 <?php
+
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function (Request $request) {
     return response()->json(["deucerto" => "true"]);
+});
+
+Route::post("/login",  [AuthController::class, "logginAttempt"]);
+
+Route::prefix('/user')->group(function () {
+    // Defina suas rotas aqui
+    Route::post('/create', [AuthController::class, 'store']);
 });
