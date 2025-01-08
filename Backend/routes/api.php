f3c6165fa23ecf199af9598b24bf92b5a3648bab
gabrielpinheirofernandes <gab.pin.fer@hotmail.com>
2025-01-08 03:21:44 -0300

    add:api route && AuthController

---
+++ Backend/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get("/", function (Request $request) {
+    return response()->json(["deucerto" => "true"]);
+});
